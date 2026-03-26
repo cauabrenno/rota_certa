0d21a9d6ea1dc94210c1d79974d47be871802d7b
Nadson Klaus <klausplima@gmail.com>
2026-03-25 22:54:50 -0300

    feat: criação do endpoint de pedidos com JWT

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PedidoController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('/pedidos', [PedidoController::class, 'store']);
