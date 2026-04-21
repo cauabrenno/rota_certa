5ea88748bc0a7de908a8f3898d6f62a52cc01292
Cauã Brenno <cauabrenno007@gmail.com>
2026-04-21 20:36:03 -0300

    Mudança na API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,23 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // URL padrão do Laravel rodando localmente. 
-  // Se o back-end configurou diferente, depois ajustamos!
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   }
+})
+
+// Aqui entra a mágica: O Interceptor
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token') // Pega o crachá
+  
+  if (token) {
+    // Se tiver crachá, anexa na requisição para o Laravel liberar a porta
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  
+  return config
 })
 
 export default api
