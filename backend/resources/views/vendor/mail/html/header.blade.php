b9fa3ab898049695dfb4b66878eda4ed8b00b26e
Cauã Brenno <cauabrenno007@gmail.com>
2026-05-05 21:40:58 -0300

    .

--- backend/resources/views/vendor/mail/html/header.blade.php
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,12 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block; color: #333; text-decoration: none; font-size: 24px; font-weight: bold;">
-    RotaCerta
-</a>
+<td class="header" style="text-align: center; padding: 20px 0;">
+    <!-- O href aqui é para onde o clique vai levar (URL do seu site) -->
+    <a href="{{ config('app.url') }}" style="display: inline-block; text-decoration: none;">
+        
+        <!-- O src com asset() é o que puxa a imagem da pasta public/images -->
+        <img src="{{ asset('images/rotaLogo.png') }}" alt="RotaCerta" style="max-height: 60px; width: auto; display: block;">
+        
+    </a>
 </td>
 </tr>
